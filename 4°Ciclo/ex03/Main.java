4cd826ea0bbb6bdf2d4f7102fdd4a66d9965d65f
Lucas Conceição Ricardo <161897897+Lucas-Conceicao@users.noreply.github.com>
2025-08-28 17:40:38 -0300

    Create Main.java

---
+++ 4°Ciclo/ex03/Main.java
@@ -0,0 +1,15 @@
+package ex04;
+
+public class Main {
+    public static void main(String[] args) {
+        Complexo a = new Complexo(1.0,6.0);
+        Complexo b = new Complexo(3.0,2.0);
+        Complexo soma = a.soma(b);
+        Complexo multiplica = a.multiplica(b);
+
+        System.out.println("Resultado da soma dos números: " + soma);
+        System.out.println("Resultado da multiplicacao dos números: " + multiplica);
+        System.out.println("Módulo de " + a + ": "+ a.modulo());
+        System.out.println("Módulo de " + b + ": "+ b.modulo());
+    }
+}
